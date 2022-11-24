1b84193412fb917250ac28f4b6955c07d038684f
Derzhavin <derzhavind@bk.ru>
2022-11-24 18:07:50 +0300

    added script for training BERT

---
+++ src/neural_networks/sentiment_estimation/train_BERT.py
@@ -0,0 +1,213 @@
+import time
+import torch.optim as optim
+import torch
+import torch.nn as nn
+from torchtext.legacy import data
+import random
+import numpy as np
+from torchtext.legacy import datasets
+from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
+from BERT import BERTGRUSentiment
+
+
+N_EPOCHS = 4
+BATCH_SIZE = 32
+
+HIDDEN_DIM = 128
+OUTPUT_DIM = 1
+N_LAYERS = 1
+BIDIRECTIONAL = True
+DROPOUT = 0.25
+
+SEED = 1234
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+
+init_token = tokenizer.cls_token
+eos_token = tokenizer.sep_token
+pad_token = tokenizer.pad_token
+unk_token = tokenizer.unk_token
+
+init_token_idx = tokenizer.convert_tokens_to_ids(init_token)
+eos_token_idx = tokenizer.convert_tokens_to_ids(eos_token)
+pad_token_idx = tokenizer.convert_tokens_to_ids(pad_token)
+unk_token_idx = tokenizer.convert_tokens_to_ids(unk_token)
+
+init_token_idx = tokenizer.cls_token_id
+eos_token_idx = tokenizer.sep_token_id
+pad_token_idx = tokenizer.pad_token_id
+unk_token_idx = tokenizer.unk_token_id
+
+
+max_input_length = tokenizer.max_model_input_sizes['bert-base-uncased']
+
+
+def tokenize_and_cut(sentence):
+    tokens = tokenizer.tokenize(sentence)
+    tokens = tokens[:max_input_length - 2]
+    return tokens
+
+
+TEXT = data.Field(batch_first=True,
+                  use_vocab=False,
+                  tokenize=tokenize_and_cut,
+                  preprocessing=tokenizer.convert_tokens_to_ids,
+                  init_token=init_token_idx,
+                  eos_token=eos_token_idx,
+                  pad_token=pad_token_idx,
+                  unk_token=unk_token_idx)
+
+LABEL = data.LabelField(dtype=torch.float)
+
+
+train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+
+print(f"Number of training examples: {len(train_data)}")
+print(f"Number of validation examples: {len(valid_data)}")
+print(f"Number of testing examples: {len(test_data)}")
+
+
+tokens = tokenizer.convert_ids_to_tokens(vars(train_data.examples[6])['text'])
+
+LABEL.build_vocab(train_data)
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size=BATCH_SIZE,
+    device=device)
+
+
+bert = BertModel.from_pretrained('bert-base-uncased')
+
+model = BERTGRUSentiment(bert,
+                         HIDDEN_DIM,
+                         OUTPUT_DIM,
+                         N_LAYERS,
+                         BIDIRECTIONAL,
+                         DROPOUT)
+
+
+for name, param in model.named_parameters():
+    if name.startswith('bert'):
+        param.requires_grad = False
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+
+optimizer = optim.Adam(model.parameters())
+criterion = nn.BCEWithLogitsLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    # round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float()  # convert into float for division
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+def train(model, iterator, optimizer, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+
+    for batch in iterator:
+        optimizer.zero_grad()
+
+        predictions = model(batch.text).squeeze(1)
+
+        loss = criterion(predictions, batch.label)
+
+        acc = binary_accuracy(predictions, batch.label)
+
+        loss.backward()
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        for batch in iterator:
+            predictions = model(batch.text).squeeze(1)
+
+            loss = criterion(predictions, batch.label)
+
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+
+    start_time = time.time()
+
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tut6-model1.pt')
+
+    print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
+
+
+model.load_state_dict(torch.load('tut6-model.pt'))
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc * 100:.2f}%')
